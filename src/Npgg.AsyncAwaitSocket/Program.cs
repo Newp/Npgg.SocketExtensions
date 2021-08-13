619fdf99bf3a3d18edc1e99a64826629325b0afa
Newp <mynewp@gmail.com>
2021-08-14 07:20:32 +0900

    socket recv 확장함수 추가

---
+++ src/Npgg.AsyncAwaitSocket/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using BinaryEncoding;
+
+namespace Npgg.AsyncAwaitSocket
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var listener = new TcpListener(new IPEndPoint(IPAddress.Any, 8088));
+            listener.Start();
+
+            Log("listener started");
+            
+            while (true)
+            {
+                var client = await listener.AcceptTcpClientAsync().ConfigureAwait(false);
+                _ = Handle(client.Client);
+                Log("connected");
+            }
+        }
+
+        static async Task Handle(Socket socket)
+        {
+            while (true)
+            {
+                var header = await socket.ReceiveAsync(2);
+
+                var payloadLength = Binary.LittleEndian.GetInt16(header);
+
+                var payload = await socket.ReceiveAsync(payloadLength);
+
+                var message = Encoding.UTF8.GetString(payload).Trim();
+
+                Log(message);
+            }
+        }
+
+        static void Log(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now:HHmmss}] : {message}");
+        }
+
+    }
+}
---
+++ src/Npgg.AsyncAwaitSocket/SocketExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Npgg.AsyncAwaitSocket
+{
+    public static class SocketExtensions
+    {
+        public static async Task<byte[]> ReceiveAsync(this Socket socket, int length)
+        {
+            byte [] buffer = new byte[length];
+            int receiveCount = await Task.Factory.FromAsync(
+                       socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, null, socket),
+                       socket.EndReceive);
+
+            if(receiveCount == 0)
+            {
+                throw new Exception("recv 0");
+            }
+
+            if(receiveCount != length)
+            {
+
+            }
+            return buffer;
+        }
+    }
+}
