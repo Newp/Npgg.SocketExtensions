c064ea4ab664a8dd4c95a0aa5efa7ab742b04c40
Newp <mynewp@gmail.com>
2021-08-14 07:52:45 +0900

    SocketExtensions 프로젝트 분리

--- src/Npgg.SocketExtensions/SocketExtensions.cs
+++ src/Npgg.SocketExtensions/SocketExtensions.cs
@@ -5,27 +5,39 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Npgg.AsyncAwaitSocket
+namespace Npgg.SocketExtensions
 {
     public static class SocketExtensions
     {
         public static async Task<byte[]> ReceiveAsync(this Socket socket, int length)
         {
-            byte [] buffer = new byte[length];
+            byte[] buffer = new byte[length];
             int receiveCount = await Task.Factory.FromAsync(
                        socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, null, socket),
                        socket.EndReceive);
 
-            if(receiveCount == 0)
+            if (receiveCount == 0)
             {
                 throw new Exception("recv 0");
             }
 
-            if(receiveCount != length)
+            if (receiveCount != length)
             {
 
             }
             return buffer;
+        }
+        public static async Task<int> SendAsync(this Socket socket, byte[] buffer)
+        {
+            int sendCount = await Task.Factory.FromAsync(
+                       socket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, null, socket),
+                       socket.EndSend);
+
+            if (sendCount != buffer.Length)
+            {
+
+            }
+            return sendCount;
         }
     }
 }
